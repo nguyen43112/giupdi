32a10ade8c7a73df7f355c976da606b6cb66a501
nguyen <nguyencbvn11@gmail.com>
2020-02-27 08:22:58 +0700

    moi

---
+++ app/src/main/java/com/example/http/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package com.giahai.thinet;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+
+    Call<List<Post>> getPost();
+}
---
+++ app/src/main/java/com/example/http/Post.java
@@ -0,0 +1,25 @@
+package com.giahai.thinet;
+
+public class Post {
+        private int userId;
+        private int id;
+        private String title;
+        private String completed;
+
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getCompleted() {
+            return completed;
+        }
+}
